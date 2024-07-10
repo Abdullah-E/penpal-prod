f2ebd1a2913a6bba4f7e09bffe6466a6939578ab
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-07-10 16:29:54 +0500

    firebase custom claims

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,19 +1,28 @@
 import { fastify, firebaseApp, BASE_URL } from "./init.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import admin from 'firebase-admin'
 import bcrypt from 'bcrypt'
-const auth = getAuth(firebaseApp);
 
+admin.initializeApp({
+    credential: admin.credential.applicationDefault()
+})
+
+const auth = getAuth(firebaseApp);
 
 import User from '../models/user.js'
 
 // import {BASE_URL} from '../server.js'
 
 fastify.post(BASE_URL+'/user', async (request, reply) => {
+    console.log(request.query)
+    const role = request.query.role || 'user'
     const { email, password, fullname } = request.body;
     try {
         const hashPass = await bcrypt.hash(password, 10)
         console.log(hashPass)
         const fb_user = await createUserWithEmailAndPassword(auth, email, password)
+        await admin.auth().setCustomUserClaims(fb_user.user.uid, {role})
+
         const user = await User.create({
             email,
             password: hashPass,
