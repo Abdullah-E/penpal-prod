094ccfa1e9c5a6252c60b3ce5c0fbfe8015989f7
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-07-13 01:49:49 +0500

    bio added

--- server/models/user.js
+++ server/models/user.js
@@ -37,6 +37,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: false
     },
+    bio:{
+        type: String,
+        required: false
+    },
     firebaseUid: {
         type: String,
         required: true
