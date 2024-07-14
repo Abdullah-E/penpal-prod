d0862c43f91ce53e7b2f80636968072602e0bcd2
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-07-15 00:48:50 +0500

    dummy customer api and testing

--- server/models/customer.js
+++ server/models/customer.js
@@ -69,6 +69,12 @@ const customerSchema = new mongoose.Schema({
         type:String,
         enum:EYE_TYPES,
         required:true
+    },
+
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
     }
 })
 
--- tests/api_test.py
+++ tests/api_test.py
@@ -2,6 +2,8 @@ import requests
 import pytest
 import random
 from faker import Faker
+import datetime
+
 
 BASE_URL = 'http://localhost:8000/api/v1'  # Replace with your server's URL
 fake = Faker()
@@ -43,3 +45,52 @@ def test_create_user(user_data):
     
     # Check that the password is not in the response data
     assert 'password' not in response_data['data']
+
+
+HAIR_TYPES = ["Bald", "Black", "Blonde", "Brown", "Gray", "Red", "Salt and Pepper", "Other"]
+EYE_TYPES = [
+    "Black" ,
+    "Blue" ,
+    "Brown" ,
+    "Green",
+    "Hazel",
+    "Other" ,
+    ]
+@pytest.fixture
+def customer_data():
+    return {
+        "firstName": fake.first_name(),
+        "lastName": fake.last_name(),
+        "inmateNumber": str(fake.random_int(min=1000, max=9999)),
+        "mailingAddress": fake.address(),
+        "city": fake.city(),
+        "state": fake.state(),
+        "zipcode": fake.zipcode(),
+        "dateOfBirth": fake.date_of_birth().isoformat(),
+        "height": fake.random_int(min=50, max=100),
+        "weight": fake.random_int(min=50, max=100),
+        "hairColor": random.choice(HAIR_TYPES),
+        "eyeColor": random.choice(EYE_TYPES),
+        
+    }
+
+def test_create_customer(customer_data):
+    response = requests.post(f"{BASE_URL}/customer", json=customer_data)
+    
+    assert response.status_code == 201, f"Expected status code 201, got {response.status_code} , response: {response.json()}"
+    
+    assert response.json()['status_code'] == 201
+    assert response.json()['message'] == "Customer created successfully"
+    assert 'data' in response.json()
+    assert 'firstName' in response.json()['data']
+    assert 'lastName' in response.json()['data']
+    assert 'inmateNumber' in response.json()['data']
+    assert 'mailingAddress' in response.json()['data']
+    assert 'city' in response.json()['data']
+    assert 'state' in response.json()['data']
+    assert 'zipcode' in response.json()['data']
+    assert 'dateOfBirth' in response.json()['data']
+    assert 'height' in response.json()['data']
+    assert 'weight' in response.json()['data']
+    assert 'hairColor' in response.json()['data']
+    assert 'eyeColor' in response.json()['data']
