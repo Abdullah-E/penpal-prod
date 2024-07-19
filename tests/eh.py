252fd4fea55bd73f83bc6b3d54f10441eadcd64b
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-07-19 05:26:10 +0500

    updating ratings and profilePic to schema

--- tests/eh.py
+++ tests/eh.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    auth_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImMxNTQwYWM3MWJiOTJhYTA2OTNjODI3MTkwYWNhYmU1YjA1NWNiZWMiLCJ0eXAiOiJKV1QifQ.eyJyb2xlIjoidXNlciIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9hd2F5b3V0LTU1ZmUyIiwiYXVkIjoiYXdheW91dC01NWZlMiIsImF1dGhfdGltZSI6MTcyMTMzMzM5MywidXNlcl9pZCI6InkzTzlLcEJyenpNVFZDeXpsVXFlNVFFTkZUOTIiLCJzdWIiOiJ5M085S3BCcnp6TVRWQ3l6bFVxZTVRRU5GVDkyIiwiaWF0IjoxNzIxMzMzMzkzLCJleHAiOjE3MjEzMzY5OTMsImVtYWlsIjoiaG1tbUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiaG1tbUBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.ITRLMc5j4p9PnO3IPWDLx8MvYmZY4uMHKzzaq4ZtuvzEpSQ8g15VLiegbAn2EFJrj-xcFmYT0IeMTmLIWJ3hgrtauXGCCeBoCIYPxt1qsrVPVcs1Wzhi6IFsTxvo82daNxwBd9domEQgP1ILm7nmkETzkh-JcarfVMr-tGAC3KdPLcSrtaRoQ1W7wFiedOw1RuytW0WKc55x1GsPLZSEvegBbmfODs5tezIuX2LyQkhGUI-oR8npfNRvBi2yrOYDUivrasSgMhSThZGvf16uWjx-EbJsjTLd-oRROFvKTeJ-wHXgf9MlqqZMCIimqsyHHFMqusms0XbWU7IESV7pcA"
+    auth_token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImMxNTQwYWM3MWJiOTJhYTA2OTNjODI3MTkwYWNhYmU1YjA1NWNiZWMiLCJ0eXAiOiJKV1QifQ.eyJyb2xlIjoidXNlciIsImlzcyI6Imh0dHBzOi8vc2VjdXJldG9rZW4uZ29vZ2xlLmNvbS9hd2F5b3V0LTU1ZmUyIiwiYXVkIjoiYXdheW91dC01NWZlMiIsImF1dGhfdGltZSI6MTcyMTM0Nzk5OSwidXNlcl9pZCI6InkzTzlLcEJyenpNVFZDeXpsVXFlNVFFTkZUOTIiLCJzdWIiOiJ5M085S3BCcnp6TVRWQ3l6bFVxZTVRRU5GVDkyIiwiaWF0IjoxNzIxMzQ3OTk5LCJleHAiOjE3MjEzNTE1OTksImVtYWlsIjoiaG1tbUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiaG1tbUBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.PX0a_LNrY55879lGP4nLI4C-LB2YgWkznk9frCuOPDOO6mrCoAtkMtuNjERgstXO5ZcvduHcD34nQXKg6-J0-K14hMAJXZQkNPy7v8aZsy8-XggfUNtr_KNw-5TJkfTPc-xxkx3phDTr1omzMOb8m7Z0slYft6bEo3-3LLth0SSLnxLLCOD6xzhPldE2Dy_eWNRrU_nx76v3qSW02bL8EawGFc_toL2_iNDthqr2EWpVxvD8R0wsccbXH-EOTWeU5ZN6Dy2KhlIomKHIqr35BQcJDdVNhmVw8wZ71VlKr2nDnStXwTq7hAWxfl-Et_Z6sAiYQLji9rIh_PU1tItfCA"
     
     race_options = [
         "American Indian/Alaskan Native",
@@ -51,17 +51,34 @@ if __name__ == "__main__":
     customersList = all_customers["data"]
     
     for cust in customersList:
-        print(f'updating: {cust["firstName"]} {cust["lastName"]}')
+        print(f"Updating {cust['firstName']}")
+        rating = 0
+        ratingReal = 0
+        numRatings = 0
+        
+        try:
+            numRatings = cust["numRatings"]
+        except KeyError:
+            numRatings = 0
+        try:
+            ratingReal = cust["ratingReal"]
+        except KeyError:
+            try:
+                ratingReal = cust["rating"]
+                rating = cust["rating"]
+            except KeyError:
+                ratingReal = 0
+                rating = 0
+            
         
         req_body = {
-            # "gender": random.choice(gender_options),
-            # "orientation": random.choice(orientation_options),
-            "race": random.choice(race_options),
-            "education": random.choice(edu_options),
-            "age":str(random.randint(18, 60)),
+            "profilePic":"",
+            "ratingReal":ratingReal,
+            "rating":rating,
+            "numRatings":numRatings,
         }
         
         put_response = requests.put(f"{BASE_URL}/customer/test?id={cust['_id']}", headers={"Authorization":f"Bearer {auth_token}"},json=req_body)
-        print(put_response.json())
+        # print(put_response.json())
     
     
